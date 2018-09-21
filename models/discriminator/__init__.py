a3f0eaef2d7499ad07e7cab4763e9292e3e41a86
flystarhe <flystarhe@qq.com>
2018-09-21 13:51:17 +0800

    update: models/discriminator/__init__.py

--- models/discriminator/__init__.py
+++ models/discriminator/__init__.py
@@ -11,11 +11,13 @@ class Discriminator(nn.Module):
         layers = list()
         layers.append(nn.Conv2d(input_nc, conv_dim, kernel_size=4, stride=2, padding=1))
         layers.append(nn.LeakyReLU(0.1))
+
         curr_dim = conv_dim
         for i in range(1, repeat_num):
-            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1))
+            next_dim = curr_dim * 2
+            layers.append(nn.Conv2d(curr_dim, next_dim, kernel_size=4, stride=2, padding=1))
             layers.append(nn.LeakyReLU(0.1))
-            curr_dim = curr_dim * 2
+            curr_dim = next_dim
 
         self.main = nn.Sequential(*layers)
         self.final = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
@@ -29,17 +31,19 @@ class Discriminator(nn.Module):
 class Classifier(nn.Module):
     """Discriminator network with PatchGAN."""
 
-    def __init__(self, image_size=128, input_nc=3, conv_dim=64, repeat_num=6, class_num=2):
+    def __init__(self, image_size=512, input_nc=3, conv_dim=64, repeat_num=6, class_num=2):
         super(Classifier, self).__init__()
 
         layers = list()
         layers.append(nn.Conv2d(input_nc, conv_dim, kernel_size=4, stride=2, padding=1))
         layers.append(nn.LeakyReLU(0.1))
+
         curr_dim = conv_dim
         for i in range(1, repeat_num):
-            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=4, stride=2, padding=1))
+            next_dim = curr_dim * 2
+            layers.append(nn.Conv2d(curr_dim, next_dim, kernel_size=4, stride=2, padding=1))
             layers.append(nn.LeakyReLU(0.1))
-            curr_dim = curr_dim * 2
+            curr_dim = next_dim
 
         self.main = nn.Sequential(*layers)
         self.final = nn.Linear(int(np.power(image_size / np.power(2, repeat_num), 2)), class_num)
