705386a5c336523e3350e68f7cd488c28656a61b
flystarhe <flystarhe@qq.com>
2018-10-26 12:47:08 +0800

    update: train_paired.py

---
+++ train_paired.py
@@ -0,0 +1,10 @@
+start_iters = 1
+train_iters = 1
+updates_g = 1
+updates_d = 1
+
+for t in range(start_iters, start_iters + train_iters):
+    for k in range(updates_g):
+        pass
+    for k in range(updates_d):
+        pass
